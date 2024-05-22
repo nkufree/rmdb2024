5f1c68cf9fac5b90ae2b4202452cc29ccec7f14e
2110853 <2995507298@qq.com>
2024-05-22 19:47:06 +0000

    完成第一题

--- rmdb/src/record/rm_scan.cpp
+++ rmdb/src/record/rm_scan.cpp
@@ -18,7 +18,17 @@ See the Mulan PSL v2 for more details. */
 RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
     // Todo:
     // 初始化file_handle和rid（指向第一个存放了记录的位置）
-
+    int i = 1;
+    RmPageHandle page_handle = file_handle_->fetch_page_handle(i);
+    while (page_handle.page_hdr->num_records == 0)
+    {
+        file_handle_->buffer_pool_manager_->unpin_page(page_handle.page->get_page_id(), false);
+        i++;
+        page_handle = file_handle_->fetch_page_handle(i);
+    }
+    int slot_no = Bitmap::first_bit(1, page_handle.bitmap, file_handle_->file_hdr_.num_records_per_page);
+    rid_ = {i, slot_no};
+    file_handle_->buffer_pool_manager_->unpin_page(page_handle.page->get_page_id(), false);
 }
 
 /**
@@ -27,7 +37,28 @@ RmScan::RmScan(const RmFileHandle *file_handle) : file_handle_(file_handle) {
 void RmScan::next() {
     // Todo:
     // 找到文件中下一个存放了记录的非空闲位置，用rid_来指向这个位置
-
+    const int& max_no = file_handle_->file_hdr_.num_records_per_page;
+    RmPageHandle page_handle = file_handle_->fetch_page_handle(rid_.page_no);
+    int slot_no;
+    while(true)
+    {
+        slot_no = Bitmap::next_bit(1, page_handle.bitmap, max_no, rid_.slot_no);
+        if(slot_no < max_no)
+        {
+            rid_ = {rid_.page_no, slot_no};
+            file_handle_->buffer_pool_manager_->unpin_page(page_handle.page->get_page_id(), false);
+            break;
+        }
+        file_handle_->buffer_pool_manager_->unpin_page(page_handle.page->get_page_id(), false);
+        int i = rid_.page_no + 1;
+        if(i >= file_handle_->file_hdr_.num_pages)
+        {
+            rid_ = {i, -1};
+            break;
+        }
+        page_handle = file_handle_->fetch_page_handle(i);
+        rid_ = {i, -1};
+    }
 }
 
 /**
@@ -35,8 +66,7 @@ void RmScan::next() {
  */
 bool RmScan::is_end() const {
     // Todo: 修改返回值
-
-    return false;
+    return rid_.slot_no == -1;
 }
 
 /**
