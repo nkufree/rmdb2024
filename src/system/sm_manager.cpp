c99b97076563fccdd820d9db2deb3e5d3d498aa4
2110853 <2995507298@qq.com>
2024-05-26 12:00:08 +0000

    修改文件位置

--- src/system/sm_manager.cpp
+++ src/system/sm_manager.cpp
@@ -88,9 +88,9 @@ void SmManager::open_db(const std::string& db_name) {
     if (!is_dir(db_name)) {
         throw DatabaseNotFoundError(db_name);
     }
-    // if (chdir(db_name.c_str()) < 0) {  // 进入名为db_name的目录
-    //     throw UnixError();
-    // }
+    if (chdir(db_name.c_str()) < 0) {  // 进入名为db_name的目录
+        throw UnixError();
+    }
     std::ifstream ifs(DB_META_NAME);
     ifs >> db_;
     // 打开所有表的文件
