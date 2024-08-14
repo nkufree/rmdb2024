6193de4feaa248110de41a8b1242a81ccb770851
2110853 <2995507298@qq.com>
2024-08-14 10:52:59 +0000

    测试索引

--- src/index/ix_index_handle.cpp
+++ src/index/ix_index_handle.cpp
@@ -579,7 +579,7 @@ void IxIndexHandle::redistribute(IxNodeHandle *neighbor_node, IxNodeHandle *node
     // 2. 从neighbor_node中移动一个键值对到node结点中
     // 3. 更新父节点中的相关信息，并且修改移动键值对对应孩字结点的父结点信息（maintain_child函数）
     // 注意：neighbor_node的位置不同，需要移动的键值对不同，需要分类讨论
-    // std::cout << "Redistribute (neighbour, node): " << std::endl;
+    std::cout << "Redistribute (neighbour, node): " << std::endl;
     // neighbor_node->print_info();
     // node->print_info();
     if(index == 0)
@@ -605,7 +605,7 @@ void IxIndexHandle::redistribute(IxNodeHandle *neighbor_node, IxNodeHandle *node
     // std::cout << "Redistribute result: " << std::endl;
     // neighbor_node->print_info();
     // node->print_info();
-    // std::cout << "End Redistribute" << std::endl;
+    std::cout << "End Redistribute" << std::endl;
 }
 
 /**
@@ -629,7 +629,7 @@ bool IxIndexHandle::coalesce(IxNodeHandle **neighbor_node, IxNodeHandle **node, 
     // 2. 把node结点的键值对移动到neighbor_node中，并更新node结点孩子结点的父节点信息（调用maintain_child函数）
     // 3. 释放和删除node结点，并删除parent中node结点的信息，返回parent是否需要被删除
     // 提示：如果是叶子结点且为最右叶子结点，需要更新file_hdr_.last_leaf
-    // std::cout << "coalesce (neighbour, node, parent): " << std::endl;
+    std::cout << "coalesce (neighbour, node, parent): " << std::endl;
     // (*neighbor_node)->print_info();
     // (*node)->print_info();
     // (*parent)->print_info();
@@ -664,7 +664,7 @@ bool IxIndexHandle::coalesce(IxNodeHandle **neighbor_node, IxNodeHandle **node, 
     // std::cout << "coalesce result (neighbour, parent): " << std::endl;
     // (*neighbor_node)->print_info();
     // (*parent)->print_info();
-    // std::cout << "End coalesce" << std::endl;
+    std::cout << "End coalesce" << std::endl;
     return res;
 }
 
